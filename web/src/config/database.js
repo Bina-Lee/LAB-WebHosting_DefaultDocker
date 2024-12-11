6b7ecb63fc9b166a49f62d83cdfa690f54eb7f90
Rimi <lora3226@inu.ac.kr>
2024-12-12 01:58:26 +0900

    ✨[feat] database 연동

--- web/src/config/database.js
+++ web/src/config/database.js
@@ -1,15 +1,13 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import mysql from 'mysql2/promise';
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-const config = {
-    host: `${process.env.DB_HOST}`,
-    user: `${process.env.DB_USER}`,
-    port: `${process.env.DB_PORT}`,
-    password: `${process.env.DB_PASS}`,
-    database: `${process.env.DB_NAME}`,
-};
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 
-const pool = mysql.createPool(config);
-
-export default pool
+module.exports = pool;
